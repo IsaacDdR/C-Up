92e2e4a24216fb9625cd929229f12f92f1893535
IsaacDdR <isaacovic00@gmail.com>
2018-10-19 13:57:16 -0500

    C-Shell improvements

---
+++ C-Shell/main.c
@@ -0,0 +1,23 @@
+void lsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    print("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free (line);
+    free (args);
+  }while(status);
+}
+
+int main(int argc, char **argv)
+{
+  lsh_loop();
+
+  return EXIT_SUCCESS;
+}
