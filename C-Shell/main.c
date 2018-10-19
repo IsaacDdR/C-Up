85812990bd1fac842fba4cd6d32fe681da7722e1
IsaacDdR <isaacovic00@gmail.com>
2018-10-19 18:57:51 -0500

    Continuacion Shell

--- C-Shell/main.c
+++ C-Shell/main.c
@@ -1,3 +1,34 @@
+#define LSH_RL_BUFSIZE 1024
+
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer){
+    fprint(stderr, "lsh: allocation error \n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1){
+    c = getchar();
+
+    if (c === EOF || c == '\n') {
+      buffer[positon] = '\0';
+      return buffer;
+    }else {
+      buffer[position] = c;
+    }
+    position ++;
+
+    if (position >= bufsize){
+      bufsize += 
+    }
+  }
+}
+
 void lsh_loop(void)
 {
   char *line;
@@ -14,6 +45,8 @@ void lsh_loop(void)
     free (args);
   }while(status);
 }
+
+
 
 int main(int argc, char **argv)
 {
