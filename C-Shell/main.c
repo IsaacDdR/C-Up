05d9d7940d9741c02b3a2e70bfcefe56ed5c863b
IsaacDdR <isaacovic00@gmail.com>
2018-10-22 15:37:01 -0500

    Finish C-Shell

--- C-Shell/main.c
+++ C-Shell/main.c
@@ -1,3 +1,52 @@
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[])(char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL){
+    fprintf(stderr, "lsh: expected argument to \'cd\'n");
+  } else {
+    if (chdir(args[1]) != 0){
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int lsh_help(char **args){
+  int i;
+  printf("Isaac Diaz LSH\n");
+  printf("Type program names and arguments, and hit enter. \n");
+  printf("The following are built in: \n");
+
+  for (i = 0; i < lsh_num_builtins; i++){
+      printf('&s\n', builtin_str[1]);
+  }
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args){
+  return 0;
+}
+
 int lsh_launch(char **args){
   pid_t pid, wpid;
   int status;
@@ -104,10 +153,6 @@ char **lsh_split_line(char *line)
   return tokens;
 }
 
-
-
-
-
 void lsh_loop(void)
 {
   char *line;
@@ -124,8 +169,6 @@ void lsh_loop(void)
     free (args);
   }while(status);
 }
-
-
 
 int main(int argc, char **argv)
 {
